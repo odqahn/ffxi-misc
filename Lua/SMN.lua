5c2cabbd0a8a83d4257294763d4adab9ee8355b0
Simon Michelizza <simon@agaru.be>
2023-04-03 18:32:44 +0100

    Added SMN

---
+++ Lua/SMN.lua
@@ -0,0 +1,145 @@
+local profile = {};
+local sets = {
+    ['Normal'] = {
+        Main = 'Chatoyant Staff',
+        Sub = 'Staff Strap',
+        Ammo = 'Hedgehog Bomb',
+        Neck = 'Smn. Torque',
+        Ear1 = 'Star Earring',
+        Ear2 = 'Star Earring',
+        Body = 'Vermillion Cloak',
+        Hands = 'Summoner\'s Brcr.',
+        Ring1 = 'Warp Ring',
+        Ring2 = 'Evoker\'s Ring',
+        Back = 'Errant Cape',
+        Waist = 'Jungle Rope',
+        Legs = 'Summoner\'s Spats',
+        Feet = 'Summoner\'s Pgch.',
+    },
+    ['Mog'] = {
+        Body = 'Kupo Suit',
+    },
+};
+profile.Sets = sets;
+
+local Settings = {
+    Mog = false;
+};
+
+-- Sleep for lockstyleset
+local clock = os.clock;
+function Sleep(n)
+    local t0 = clock();
+    while clock() - t0 <= n do end;
+end
+
+profile.Packer = {
+};
+
+profile.OnLoad = function()
+    gSettings.AllowAddSet = true;
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro book 8');
+	AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
+	AshitaCore:GetChatManager():QueueCommand(1, '/echo SMN loading!');
+    -- Sleep(1);
+	-- AshitaCore:GetChatManager():QueueCommand(1, '/lockstyleset 4 echo');
+end
+
+profile.OnUnload = function()
+end
+
+profile.HandleCommand = function(args)
+    if (args[1] == 'mog') then
+        if (Settings.Mog == true) then
+            Settings.Mog = false;
+            gFunc.Message('Mog Set Off');
+        else
+            Settings.Mog = true;
+            gFunc.Message('Mog Set On');
+        end
+    end
+    if (args[1] == 'siphon') then
+        local game = gData.GetEnvironment();
+        local spirit = "unknown";
+        if(game.Weather:contains("x2")) then
+            if(game.Weather == "Fire x2") then
+                spirit = "Fire Spirit";
+            elseif(game.Weather == "Water x2") then
+                spirit = "Water Spirit";
+            elseif(game.Weather == "Earth x2") then
+                spirit = "Earth Spirit";
+            elseif(game.Weather == "Wind x2") then
+                spirit = "Wind Spirit";
+            elseif(game.Weather == "Ice x2") then
+                spirit = "Ice Spirit";
+            elseif(game.Weather == "Thunder x2") then
+                spirit = "Thunder Spirit";
+            elseif(game.Weather == "Light x2") then
+                spirit = "Light Spirit";
+            elseif(game.Weather == "Dark x2") then
+                spirit = "Dark Spirit";
+            end
+        else
+            if(game.DayElement == "Fire") then
+                spirit = "Fire Spirit";
+            elseif(game.DayElement == "Water") then
+                spirit = "Water Spirit";
+            elseif(game.DayElement == "Earth") then
+                spirit = "Earth Spirit";
+            elseif(game.DayElement == "Wind") then
+                spirit = "Wind Spirit";
+            elseif(game.DayElement == "Ice") then
+                spirit = "Ice Spirit";
+            elseif(game.DayElement == "Thunder") then
+                spirit = "Thunder Spirit";
+            elseif(game.DayElement == "Light") then
+                spirit = "Light Spirit";
+            elseif(game.DayElement == "Dark") then
+                spirit = "Dark Spirit";
+            end
+        end
+        if(spirit ~= "unknown") then
+            AshitaCore:GetChatManager():QueueCommand(1, '/ma "' .. spirit .. '" <me> <wait5>');
+        end
+    end
+end
+
+profile.HandleDefault = function()
+    local game = gData.GetEnvironment();
+    local player = gData.GetPlayer();
+
+    if (player.Status == 'Engaged') then
+        gFunc.EquipSet(sets.Normal);
+    elseif (player.Status == 'Resting') then
+        gFunc.EquipSet(sets.Normal);
+    else
+        if (Settings.Mog == true) then
+            gFunc.EquipSet(sets.Mog);
+        else
+            gFunc.EquipSet(sets.Normal);
+        end
+    end
+end
+
+profile.HandleAbility = function()
+end
+
+profile.HandleItem = function()
+end
+
+profile.HandlePrecast = function()
+end
+
+profile.HandleMidcast = function()
+end
+
+profile.HandlePreshot = function()
+end
+
+profile.HandleMidshot = function()
+end
+
+profile.HandleWeaponskill = function()
+end
+
+return profile;
